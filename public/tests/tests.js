87804309e0409904838c019cc0787a273e7b5d01
Juan Tareq Gonzalez <alu0100603866@ull.edu.es>
2014-03-20 19:16:14 +0000

    Test 2

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -10,4 +10,13 @@ suite('ELEMENTOS', function(){
 	assert.equal(regexp.bexec(input_str), null);
   });
 
+// Probar String.tokens sobre una cadena.
+  test('Cadena de Elementos', function(){
+    var input_str = "var Laura = Tareq;";
+    var esperado_str = "[{\"type\":\"ID\",\"value\":\"var\",\"from\":0,\"to\":3},{\"type\":\"ID\",\"value\":\"Laura\",\"from\":4,\"to\":9},{\"type\":\"=\",\"value\":\"=\",\"from\":10,\"to\":11},{\"type\":\"ID\",\"value\":\"Tareq\",\"from\":12,\"to\":17},{\"type\":\";\",\"value\":\";\",\"from\":17,\"to\":18}]";
+	var resultado_str = JSON.stringify(input_str.tokens());
+
+	assert.equal(esperado_str, resultado_str);
+  });
+  
 });
